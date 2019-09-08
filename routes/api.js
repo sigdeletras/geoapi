5791cfeff0844811edef6b3d8019a0b779e6ed0e
Patricio Soriano <pasoriano@gmail.com>
2019-09-08 12:07:38 +0200

    ADD: nuevas capas para la BBDD

--- routes/api.js
+++ routes/api.js
@@ -10,5 +10,8 @@ const layer = require('../controllers/layerController')
 // Añadimos al aruta el parámetro layername para ser usado por el controlador
 router.get('/layers/:layername', layer.getGeojson)
 
+// URL para obtener el listado de capas de nuestra base de datos
+router.get('/catalog', layer.getLayersList)
+
 // Exportamos el código 
 module.exports = router;
